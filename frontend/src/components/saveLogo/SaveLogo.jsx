5d223d1b085520302d0e232b74be0870eb0d73cf
Shubham Singhania <82900163+StormSplits@users.noreply.github.com>
2024-07-31 18:01:07 +0530

    Delete frontend directory

--- frontend/src/components/saveLogo/SaveLogo.jsx
+++
@@ -1,9 +0,0 @@
-import {ReactComponent as Save } from '../../graphicalContent/saveLogo.svg'
-import './saveLogo.css'
-
-export const SaveLogo = () => {
-  return (
-    <section className='SaveLogo'><Save />
-    </section>
-  )
-}
